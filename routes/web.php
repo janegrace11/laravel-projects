d4039668633e81c1c1a3121bc52cac202434fa09
janegrace11 <janegracebautista11@gmail.com>
2025-05-17 00:02:31 +0800

    updated changes for auth in loginroute

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 
 // Home route
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Product routes
